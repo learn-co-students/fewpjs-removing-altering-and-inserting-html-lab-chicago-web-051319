e1f2b5dcbfb4529797911f15f4f9709c66b06149
Derek Townsend <derekjavon2@gmail.com>
2019-06-24 11:21:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code here!
 document.querySelector("main#main").remove();
 let newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
+// newHeader.setAttribute("id", "victory");
+newHeader.id = "victory";
 newHeader.innerHTML = "Derek is the champion";
 document.body.appendChild(newHeader);
