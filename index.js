048b93b6a9ef583cc472b0a95bb58a642af541b0
afhare <alexandra.f.hare@gmail.com>
2019-06-24 10:01:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main = document.getElementById("main");
+main.remove();
+let newHeader = document.createElement('h1');
+let header = document.createElement('header');
+document.body.appendChild(header);
+header.appendChild(newHeader);
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion";
