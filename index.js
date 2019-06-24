d89b2baebfcde8c902b4230bb08d7946098d23f4
John Linatoc <johnlinatoc1227@gmail.com>
2019-06-24 10:52:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+let para = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(para);
+document.body.appendChild(newHeader);
+// let newHeader = document.querySelector('main');
